aa5bf70bcb1ecf40e624e4584f1b3183dd08f999
CoolTrainerEX <CoolTrainerEX@gmail.com>
2026-02-06 11:45:38 +0800

    remove backend

--- app/nav.tsx
+++ app/nav.tsx
@@ -12,13 +12,15 @@ import {
   AvatarFallback,
   AvatarImage,
 } from "@/components/ui/avatar.tsx";
-import { useLogin } from "@/src/api/endpoints/default/default.ts";
 
 export default function Nav() {
-  const { data, isLoading } = useLogin({
-    email: "juan_dela_cruz@dlsu.edu.ph",
-    password: "password",
-  });
+  const data = {
+    id: 1,
+    name: {
+      first: "Juan",
+      last: "Dela Cruz",
+    },
+  };
 
   return (
     <NavigationMenu className="sticky top-0 p-4 max-w-full [&>div]:w-full bg-secondary text-secondary-foreground">
@@ -41,22 +43,20 @@ export default function Nav() {
         </NavigationMenuItem>
         <NavigationMenuItem>
           <NavigationMenuLink asChild>
-            {isLoading ? <>Loading</> : (
-              <Link
-                href={`/user/${data?.data?.id}`}
-                className="flex-row gap-4"
-              >
-                <p className="leading-7 not-first:mt-6">
-                  Hello, {data?.data?.name.first} {data?.data?.name.last}!
-                </p>
-                <Avatar>
-                  <AvatarImage src="" />
-                  <AvatarFallback>
-                    {data?.data?.name.first[0]}
-                  </AvatarFallback>
-                </Avatar>
-              </Link>
-            )}
+            <Link
+              href={`/user/${data.id}`}
+              className="flex-row gap-4 align-middle"
+            >
+              <p className="leading-7 not-first:mt-6">
+                Hello, {data.name.first} {data.name.last}!
+              </p>
+              <Avatar>
+                <AvatarImage src="" />
+                <AvatarFallback>
+                  {data.name.first[0]}
+                </AvatarFallback>
+              </Avatar>
+            </Link>
           </NavigationMenuLink>
         </NavigationMenuItem>
       </NavigationMenuList>
