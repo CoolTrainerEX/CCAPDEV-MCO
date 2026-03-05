c1a5d8578106d467b6f432e7d358379a0a04d9d2
CoolTrainerEX <CoolTrainerEX@gmail.com>
2026-03-05 14:59:53 +0800

    home

--- app/api/reservation/user/[id]/route.ts
+++ app/api/reservation/user/[id]/route.ts
@@ -9,9 +9,10 @@ import {
   UnexpectedResponse,
 } from "@/src/api/models";
 import { reservations as reservationList } from "@/src/sample";
+import { isAfter } from "date-fns";
 import { NextRequest, NextResponse } from "next/server";
 import pino from "pino";
-import { ZodError } from "zod";
+import z, { ZodError } from "zod";
 
 const PAGE_LIMIT = Number.parseInt(process.env.PAGE_LIMIT ?? "10");
 const getLogger = pino().child({ operation: "get reservations from user" });
@@ -21,19 +22,21 @@ export async function GET(
   request: NextRequest,
   context: RouteContext<"/api/reservation/user/[id]">,
 ) {
+  const now = new Date();
+
   try {
     const queryParams = ReadReservationUserQueryParams.parse(
       Object.fromEntries(request.nextUrl.searchParams.entries()),
     );
     const params = ReadReservationUserParams.parse(await context.params);
-    const reservations = reservationList
-      .filter(({ userId }) => userId === params.id)
-      .slice(
-        (queryParams.page - 1) * PAGE_LIMIT,
-        queryParams.page * PAGE_LIMIT,
-      );
+    const lastIndex = queryParams.page * PAGE_LIMIT;
+    const reservations = reservationList.filter(
+        ({ userId, schedule: { end } }) =>
+          userId === params.id && isAfter(end, now),
+      ),
+      page = reservations.slice((queryParams.page - 1) * PAGE_LIMIT, lastIndex);
 
-    if (!reservations.length) {
+    if (!page.length) {
       getLogger.info("Reservations not found.");
 
       return NextResponse.json(
@@ -44,7 +47,12 @@ export async function GET(
 
     getLogger.info("Success");
 
-    return NextResponse.json(ReadReservationUserResponse.parse(reservations));
+    return NextResponse.json(
+      ReadReservationUserResponse.parse({
+        data: page,
+        hasNextPage: lastIndex < reservations.length,
+      } as z.infer<typeof ReadReservationUserResponse>),
+    );
   } catch (e) {
     if (e instanceof ZodError) {
       getLogger.info({ issues: e.issues });
